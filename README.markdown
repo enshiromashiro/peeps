dae84707eddc0100fb5bd6ae695f396826e33f09
subaru45 <devnull@localhost>
2014-05-31 23:16:26 +0900

    Improve counting number of characters and tweeting the result

---
+++ README.markdown
@@ -0,0 +1,35 @@
+# Peeps
+
+指定したフォルダのテキストファイルの文字数をカウントして合計を出して前回実行時の合計との差を出してくれるだけでなくTwitterにつぶやいてさえくれるやつ。
+
+タスクスケジューラとかに登録しとけば、一日の終わりに文字数の変化をツイートとかできる。
+
+## Usage
+
+    peeps [調べたいフォルダ]
+
+## Installation
+
+0. TweetConsoleの準備
+1. peepsの準備
+2. タスクスケジューラへの登録
+3. 初回カウント（要手動実行）
+
+
+## Requirements
+
+* [TweetConsole](http://www.forest.impress.co.jp/docs/review/20100518_367685.html)
+  * コマンドラインTwitterクライアント
+
+## Author
+
+* subaru45
+
+## Copyright
+
+Copyright (c) 2013 subaru45
+
+# License
+
+Licensed under the [NYSL](http://www.kmonos.net/nysl/).
+
---
+++ peeps.bat
@@ -0,0 +1,88 @@
+@echo off
+@setlocal ENABLEDELAYEDEXPANSION
+
+
+@rem 内部設定いろいろ
+
+@rem Twitterクライアントのパス
+set PEEPS_TW_PATH=C:\TweetConsole
+@rem Twitterクライアントの引数（最後につぶやき文字列が連結される）
+set PEEPS_TW_CMD=twtcnsl.exe /t
+@rem つぶやきテンプレート
+set PEEPS_TWEET_TEMPLATE=dateの成果：num文字
+
+
+
+@rem 初期化とか
+set SELF_PATH=%~dp0
+set TARGET_PATH=%~dp1
+
+echo チェック対象フォルダ:%TARGET_PATH%
+
+@rem 合計計算
+set SUM=0
+for /R %TARGET_PATH% %%f in (*.txt) do @(
+    call :filesize %%f
+    set FSIZE=!ERRORLEVEL!
+    set /A SUM=SUM+FSIZE
+)
+@rem charnum(cp932) = filesize / 2
+set /A SUM="SUM/2"
+
+@rem 前回との差分計算
+set DIFF=0
+call :genfpath %TARGET_PATH%
+
+call :readstat %FPATH%
+set PREV=%ERRORLEVEL%
+
+call :updatestat %FPATH% %SUM%
+set /A DIFF=SUM-PREV
+
+echo 今回合計:%SUM% 、前回合計:%PREV% 、差:%DIFF%
+
+
+@rem つぶやく(前回実行時の文字数なし(%PREV%が-1)でないとき)
+set TWEET=!PEEPS_TWEET_TEMPLATE:num=%DIFF%!
+set TWEET=!TWEET:date=%DATE%!
+if -1 == %PREV% (
+  echo ディレクトリ %TARGET_PATH% の文字数は前回カウントされていません。
+  echo なのでツイートしません。
+) else (
+  echo %TWEET%
+  %PEEPS_TW_PATH%\%PEEPS_TW_CMD% %TWEET%
+)
+
+goto :eof
+
+
+@rem サブルーチン（笑）
+@rem ファイルサイズ
+:filesize
+exit /b %~z1
+
+@rem ファイル名生成
+:genfpath
+set FPATH=%1
+set FPATH=%FPATH:\=_%
+set FPATH=%FPATH::=_%
+set FPATH=%SELF_PATH%\stats\%FPATH%
+exit /b
+
+@rem 前回実行時の文字数の更新
+:updatestat
+echo %2 > %1
+exit /b
+
+@rem 前回実行時の文字数の探索
+:readstat
+set NUM=-1
+if exist %1 @(
+   for /F %%n in (%1) do @(
+      set NUM=%%n
+   )
+)
+exit /b %NUM%
+
+
+:eof
